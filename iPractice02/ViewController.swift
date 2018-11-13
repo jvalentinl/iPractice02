6c5013b775696018032fdfba03cea14f5921577c
Jerber Valentin <u201521654@upc.edu.pe>
2018-11-13 15:34:55 -0500

    Initial Commit

---
+++ iPractice02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPractice02
+//
+//  Created by Alumnos on 11/13/18.
+//  Copyright © 2018 upc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
